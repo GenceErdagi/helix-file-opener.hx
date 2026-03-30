0149ac4ef74c80fb396039783be774267d5e4b6e
Gence Erdagi <gence@Gence.localdomain>
2026-03-30 18:08:04 +0300

    Fix cog.scm: remove entrypoint, add dependencies

--- cog.scm
+++ cog.scm
@@ -1,4 +1,3 @@
 (define package-name 'helix-file-opener)
 (define version "0.1.0")
-(define dependencies '())
-(define entrypoint '(#:name "helix-file-opener" #:path "helix-file-opener.scm"))
+(define dependencies '(helix helix-server))
